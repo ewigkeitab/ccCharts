62737af1d3430f1f4e9b984fe9d79a7dc9992f02
cch <fallinginmyhand@outlook.com>
2024-11-08 21:26:15 +0800

    20241108-12

--- lib/BadgeChart.vue
+++ lib/BadgeChart.vue
@@ -14,39 +14,38 @@ const props = defineProps<{
 </script>
 <script lang="ts">
 export default {
-  'name': "BadgeChart",
+  name: "BadgeChart",
 };
 </script>
-<style scoped>
+<style>
 .outline {
   background-color: #fffbda;
-    border-style: solid;
-    border-width: 15px;
-    border-radius: 25px;
-    border-color: rgb(87, 190, 127);
-    margin: 5px;
-    padding: 0px;
-    max-height: -webkit-fill-available;
-
+  border-style: solid;
+  border-width: 15px;
+  border-radius: 25px;
+  border-color: rgb(87, 190, 127);
+  margin: 5px;
+  padding: 0px;
+  max-height: -webkit-fill-available;
 }
 .large_width {
   width: 96px;
   height: 96px;
 }
 .mainText {
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 10%;
-    margin-bottom: 10%;
-    font-size: xx-large;
-    text-align: center;
-    height: 60%;
-    align-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  font-size: xx-large;
+  text-align: center;
+  height: 60%;
+  align-content: center;
 }
 .secText {
-    color: black;
-    text-align: end;
-    padding: 0px;
-    margin: 5px;
+  color: black;
+  text-align: end;
+  padding: 0px;
+  margin: 5px;
 }
 </style>
